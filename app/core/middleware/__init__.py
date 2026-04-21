20cc0513fe15b34b2a6f9957089962ee32c9a587
Husniddin <agzamovhusniddin73@gmail.com>
2026-04-22 00:12:51 +0500

    feat: add middlewares

---
+++ app/core/middleware/__init__.py
@@ -0,0 +1,38 @@
+from core.config import settings
+from core.middleware.security import setup_security_middleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+
+
+def setup_middleware(app: FastAPI) -> None:
+    """Register all middlewares for application"""
+
+    # Trusted hosts (защита от Host header attacks)
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=[
+            "xizmatdev.imv.uz",
+            "xizmat.uzasbo.uz",
+            "localhost",
+            "127.0.0.1",
+            "xizmat_foreign",
+            "",
+        ],
+    )
+
+    # CORS
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS_ALLOWED_ORIGINS.split(),
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    # Sessions
+    app.add_middleware(
+        SessionMiddleware,
+        secret_key=settings.SECRET_KEY,
+    )
