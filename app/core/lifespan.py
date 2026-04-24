a1d46fc245e1142df98b558ee37278c819ca801b
Husniddin <agzamovhusniddin73@gmail.com>
2026-04-24 23:02:42 +0500

    refactor: added try except when setting up admin dashboard

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -5,7 +5,6 @@ from admin import setup_admin
 from fastapi import FastAPI
 from logging_config import setup_logging
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -13,7 +12,12 @@ logger = logging.getLogger(__name__)
 async def lifespan(app: FastAPI):
     try:
         setup_logging()
-        setup_admin(app)
+
+        try:
+            setup_admin(app)
+            logger.info("🛠 Admin mounted successfully.")
+        except Exception as admin_error:
+            logger.error(f"❌ Admin startup error: {admin_error}")
 
         logger.info("✅ App starting up...")
         yield
