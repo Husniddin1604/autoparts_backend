f4b59a65105e988006ba193d5a052895dce81b29
Husniddin <agzamovhusniddin73@gmail.com>
2026-04-25 00:44:06 +0500

    refactor: some minor changes

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,8 @@ from collections.abc import AsyncGenerator
 from typing import Callable
 
 from core.config import settings
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
 
 # create an async engine
 engine = create_async_engine(url=settings.DATABASE_URL, echo=settings.DEBUG)
