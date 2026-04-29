dde909840a25ece5784984b12900c4fdb55e05e2
Husniddin <agzamovhusniddin73@gmail.com>
2026-04-29 15:58:43 +0500

    feat: added repositories to uow

--- app/uow/impl.py
+++ app/uow/impl.py
@@ -1,7 +1,29 @@
 from collections.abc import Callable
 
-from models.example import Example
-from repositories.example import ExampleRepository
+from repositories.sqlalchemy.users import SqlAlchemyUserRepository
+from repositories.sqlalchemy.autoparts import (
+    SqlAlchemyBrandRepository,
+    SqlAlchemyPartRepository,
+    SqlAlchemyPartNumbersRepository,
+    SqlAlchemyCrossReferenceRepository,
+    SqlAlchemyPartFitmentRepository,
+)
+from repositories.sqlalchemy.cars import (
+    SqlAlchemyManufacturerRepository,
+    SqlAlchemyCarModelRepository,
+    SqlAlchemyCarModificationRepository,
+)
+from repositories.sqlalchemy.categories import (
+    SqlAlchemyCategoryRepository,
+    SqlAlchemyAttributeRepository,
+    SqlAlchemyCategoryAttributeRepository,
+    SqlAlchemyPartCategoryLinksRepository,
+)
+from repositories.sqlalchemy.products import (
+    SqlAlchemyProductRepository,
+    SqlAlchemyStockRepository,
+    SqlAlchemyProductAttributeValuesRepository,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 from uow.ports import UnitOfWorkABC
 
@@ -12,25 +34,36 @@ class UnitOfWork(UnitOfWorkABC):
 
     async def __aenter__(self):
         self._session = self._session_factory()
-        self.examle = ExampleRepository(self._session, Example)
+        self.user = SqlAlchemyUserRepository(self._session)
+        self.brand = SqlAlchemyBrandRepository(self._session)
+        self.part = SqlAlchemyPartRepository(self._session)
+        self.part_numbers = SqlAlchemyPartNumbersRepository(self._session)
+        self.cross_reference = SqlAlchemyCrossReferenceRepository(self._session)
+        self.part_fitment = SqlAlchemyPartFitmentRepository(self._session)
+        self.manufacturer = SqlAlchemyManufacturerRepository(self._session)
+        self.car_model = SqlAlchemyCarModelRepository(self._session)
+        self.car_modification = SqlAlchemyCarModificationRepository(self._session)
+        self.category = SqlAlchemyCategoryRepository(self._session)
+        self.attribute = SqlAlchemyAttributeRepository(self._session)
+        self.category_attribute = SqlAlchemyCategoryAttributeRepository(self._session)
+        self.part_category_links = SqlAlchemyPartCategoryLinksRepository(self._session)
+        self.product = SqlAlchemyProductRepository(self._session)
+        self.stock = SqlAlchemyStockRepository(self._session)
+        self.product_attribute_values = SqlAlchemyProductAttributeValuesRepository(self._session)
 
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback):
-        if exc_type:
-            await self.rollback()
-        else:
+        try:
+            if exc_type:
+                await self._session.rollback()
+            else:
+                await self._session.commit()
+        finally:
             await self._session.close()
 
     async def commit(self):
-        try:
-            await self._session.commit()
-        except Exception:
-            await self.rollback()
-            raise
+        await self._session.commit()
 
     async def rollback(self):
-        try:
-            await self._session.rollback()
-        finally:
-            await self._session.close()
+        await self._session.rollback()
--- app/uow/ports.py
+++ app/uow/ports.py
@@ -1,7 +1,48 @@
 from abc import ABC, abstractmethod
+from repositories.ports.users import UserRepository
+from repositories.ports.autoparts import (
+    BrandRepository,
+    PartRepository,
+    PartNumbersRepository,
+    CrossReferenceRepository,
+    PartFitmentRepository,
+)
+from repositories.ports.cars import (
+    ManufacturerRepository,
+    CarModelRepository,
+    CarModificationRepository,
+)
+from repositories.ports.categories import (
+    CategoryRepository,
+    AttributeRepository,
+    CategoryAttributeRepository,
+    PartCategoryLinksRepository,
+)
+from repositories.ports.products import (
+    ProductRepository,
+    StockRepository,
+    ProductAttributeValuesRepository,
+)
 
 
 class UnitOfWorkABC(ABC):
+    user: UserRepository
+    brand: BrandRepository
+    part: PartRepository
+    part_numbers: PartNumbersRepository
+    cross_reference: CrossReferenceRepository
+    part_fitment: PartFitmentRepository
+    manufacturer: ManufacturerRepository
+    car_model: CarModelRepository
+    car_modification: CarModificationRepository
+    category: CategoryRepository
+    attribute: AttributeRepository
+    category_attribute: CategoryAttributeRepository
+    part_category_links: PartCategoryLinksRepository
+    product: ProductRepository
+    stock: StockRepository
+    product_attribute_values: ProductAttributeValuesRepository
+
     @abstractmethod
     async def __aenter__(self):
         raise NotImplementedError()
