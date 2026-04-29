66c3e9614923eab4d63ba45dd0938995c61488de
Husniddin <agzamovhusniddin73@gmail.com>
2026-04-29 16:19:43 +0500

    fix: fix field names

--- app/admin/views/base.py
+++ app/admin/views/base.py
@@ -4,9 +4,9 @@ from starlette_admin.contrib.sqla import ModelView
 
 
 class BaseAdminView(ModelView):
-    exclude_fields_from_create = ["createdAt", "updatedAt"]
-    exclude_fields_from_edit = ["createdAt", "updatedAt"]
-    exclude_fields_from_list = ["createdAt", "updatedAt"]
+    exclude_fields_from_create = ["created_at", "updated_at"]
+    exclude_fields_from_edit = ["created_at", "updated_at"]
+    exclude_fields_from_list = ["created_at", "updated_at"]
 
     def can_delete(self, request: Request) -> bool:
         if settings.APP_MODE == "PRODUCTION":
